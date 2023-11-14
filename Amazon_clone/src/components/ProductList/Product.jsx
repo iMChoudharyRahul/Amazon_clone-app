853271113b0dd889f577c7f237a89951a79ff7fb
Choudhary8055 <rahulchoudhary200599@gmail.com>
2023-11-14 15:35:10 +0530

    update product section and ads section and login section

--- Amazon_clone/src/components/ProductList/Product.jsx
+++ Amazon_clone/src/components/ProductList/Product.jsx
@@ -3,9 +3,7 @@ import "./Product.css";
 import { useStateValue } from "../../context/StateProvider";
 import ProductContext from "../../context/CreateContext";
 import { Oval } from "react-loader-spinner";
-import { colors } from "@mui/material";
 
-// function Product({id,title,image,price,rating}) {
 function Product() {
   const [state, dispatch] = useStateValue();
   const { productList, loading, error } = useContext(ProductContext);
@@ -16,8 +14,6 @@ function Product() {
     const id = Number(e.target.id);
     //find which product we need to add in the cart from the productData
     const selectedProduct = productData.find((product) => product.id === id);
-    console.log("Check Select Product:", selectedProduct);
-    //  dispatch(addProduct({ product: selectedProduct }));
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
@@ -26,59 +22,53 @@ function Product() {
         image: selectedProduct.image,
         price: selectedProduct.price,
         rating: selectedProduct.rating.rate,
+        category: selectedProduct.category,
       },
     });
   };
 
   useEffect(() => {
     setProductData(productList);
-    console.log("Product Data:=", productData);
   }, [productList]);
 
+  /**
+   * loader during apis data fetching
+   */
   if (loading) {
-    console.log("Check Loading data", loading);
-    return <h2 style={{ color: "black" }}>Loading...plaese Wait</h2>;
+    return (
+      <Oval
+        height={80}
+        width={80}
+        color="green"
+        visible={true}
+        wrapperClass="oval-loading"
+        ariaLabel="oval-loading"
+        secondaryColor="#febd69"
+        strokeWidth={6}
+        strokeWidthSecondary={6}
+      />
+    );
   }
-  // if (loading){
-  //   console.log("Check Loading data", loading);
-  //   return (
-  //     // <Oval
-  //     //   height={60}
-  //     //   width={60}
-  //     //   color="#febd69"
-  //     //   visible={true}
-  //     //   wrapperClass="justify-center"
-  //     //   ariaLabel="oval-loading"
-  //     //   secondaryColor="#febd69"
-  //     //   strokeWidth={6}
-  //     //   strokeWidthSecondary={6}
-  //     // />
-  //     <h2 style={{color: "black"}}>Loading...plaese Wait</h2>
-  //   )};
 
   if (error) {
     return console.log("Something went wrong please check api");
   }
-  console.log("Check Loading data outside", loading);
 
   return (
     <>
       <div className="product__box">
-        {/* start the map from here  */}
         {productData.map((item) => (
           <div className="product__item" key={item.id}>
             <span className="item__catagory">{item.category}</span>
-            {/* ======product image====  */}
             <div className="product__img">
               <img src={item.image} alt="Laptop-beg" />
             </div>
-            {/* ========= product info start from here ======= */}
             <div className="product__info">
               <div className="product__title">
                 <h2 className="title">{item.title.substring(0, 18)}</h2>
                 <p className="product__price">
                   <small>₹</small>
-                  <strong>{Math.round(item.price * 72)}</strong>
+                  <strong>{Math.round(item.price * 83)}</strong>
                 </p>
               </div>
               <div className="product__description">
---
+++ Amazon_clone/src/constants/ProductAds.js
@@ -0,0 +1,31 @@
+import {
+  ElectronicItems,
+  MensCasuals,
+  MensFashionItems,
+  WomenFashionItems,
+} from "../assets/ExportImg";
+
+const ProductAdsData = [
+  {
+    title: "Up to 60% off | Styles for men",
+    category: "MensFashionItems",
+    image: MensFashionItems,
+  },
+  {
+    title: "Up to 60% off | Styles for women",
+    category: "WomenFashionItems",
+    image: WomenFashionItems,
+  },
+  {
+    title: "Up to 70% off | Clearance store",
+    category: "Electronics",
+    image: ElectronicItems,
+  },
+  //   {
+  //     title: "Flat 50% off | Styles for men",
+  //     category: "MensCasuals",
+  //     image: MensCasuals,
+  //   },
+];
+
+export default ProductAdsData;
